02bdb38c7e868ac8dc8311449bae2f4429fb1758
omrifahn <45762588+omrifahn@users.noreply.github.com>
2021-10-08 19:58:27 +0300

    fix the qr problem

--- src/components/QrGenerator.js
+++ src/components/QrGenerator.js
@@ -1,7 +1,6 @@
 import QRCode from 'qrcode'
-import {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import createIcsString from "./createIcsString";
-
 
 const QrGenerator = (props) => {
     let text = createIcsString(props)
@@ -11,10 +10,12 @@ const QrGenerator = (props) => {
         QRCode.toDataURL(text).then( (data) => {
             setSrc(data)
         } )
-    }, [] )
+    }, [text] )
 
     return (
         <div>
+            <p>{text}</p>
+            <p>{props.appointments[0].directors[0]}</p>
             <img width="50" height="50" src={src} />
         </div>
     )
