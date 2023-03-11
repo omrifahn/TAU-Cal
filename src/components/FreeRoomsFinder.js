a3da75d2426108bfbcb6c8e9f875a6cdfc1bfc45
omrifahn <omrifahn@gmail.com>
2023-03-11 16:48:04 +0200

    Better UI I make a stop to order things by folders and then will come back again

--- src/components/FreeRoomsFinder.js
+++ src/components/FreeRoomsFinder.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import data from './../real_rooms_availability.json';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+}));
 
 const FreeRoomsFinder = () => {
+    const classes = useStyles();
     const [selectedDay, setSelectedDay] = useState(1);
     const [selectedHour, setSelectedHour] = useState(8);
     const [freeRooms, setFreeRooms] = useState([]);
@@ -28,34 +41,45 @@ const FreeRoomsFinder = () => {
 
     return (
         <div>
-            <div>
+            <FormControl className={classes.formControl}>
                 <label htmlFor="day-select">Day:</label>
-                <select id="day-select" value={selectedDay} onChange={handleDayChange}>
-                    <option value={1}>1</option>
-                    <option value={2}>2</option>
-                    <option value={3}>3</option>
-                    <option value={4}>4</option>
-                    <option value={5}>5</option>
-                </select>
-            </div>
-            <div>
+                <Select
+                    id="day-select"
+                    value={selectedDay}
+                    onChange={handleDayChange}
+                >
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
                 <label htmlFor="hour-select">Hour:</label>
-                <select id="hour-select" value={selectedHour} onChange={handleHourChange}>
-                    <option value={8}>8:00 - 9:00</option>
-                    <option value={9}>9:00 - 10:00</option>
-                    <option value={10}>10:00 - 11:00</option>
-                    <option value={11}>11:00 - 12:00</option>
-                    <option value={12}>12:00 - 13:00</option>
-                    <option value={13}>13:00 - 14:00</option>
-                    <option value={14}>14:00 - 15:00</option>
-                    <option value={15}>15:00 - 16:00</option>
-                    <option value={16}>16:00 - 17:00</option>
-                    <option value={17}>17:00 - 18:00</option>
-                    <option value={18}>18:00 - 19:00</option>
-                    <option value={19}>19:00 - 20:00</option>
-                </select>
-            </div>
-            <button onClick={findFreeRooms}>Find Free Rooms</button>
+                <Select
+                    id="hour-select"
+                    value={selectedHour}
+                    onChange={handleHourChange}
+                >
+                    <MenuItem value={8}>8:00 - 9:00</MenuItem>
+                    <MenuItem value={9}>9:00 - 10:00</MenuItem>
+                    <MenuItem value={10}>10:00 - 11:00</MenuItem>
+                    <MenuItem value={11}>11:00 - 12:00</MenuItem>
+                    <MenuItem value={12}>12:00 - 13:00</MenuItem>
+                    <MenuItem value={13}>13:00 - 14:00</MenuItem>
+                    <MenuItem value={14}>14:00 - 15:00</MenuItem>
+                    <MenuItem value={15}>15:00 - 16:00</MenuItem>
+                    <MenuItem value={16}>16:00 - 17:00</MenuItem>
+                    <MenuItem value={17}>17:00 - 18:00</MenuItem>
+                    <MenuItem value={18}>18:00 - 19:00</MenuItem>
+                    <MenuItem value={19}>19:00 - 20:00</MenuItem>
+                </Select>
+            </FormControl>
+            <Button variant="contained" color="" onClick={findFreeRooms}>
+                Find Free Rooms
+            </Button>
+
 
             <ul style={{ direction: 'rtl'}}>
                 {freeRooms.map((freeRoom, index, allRooms) => (
