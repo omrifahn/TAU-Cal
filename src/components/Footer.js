28d29339e4bdfa6bc5e9e5107ca193370afe05c4
omrifahn <omrifahn@gmail.com>
2023-03-31 15:41:01 +0300

    thanks to gilad and GPT4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,8 @@ export class Footer extends React.PureComponent {
         let omri =  "https://www.linkedin.com/in/omri-fahn/"
         let ido = "https://www.linkedin.com/in/ido-weins"
         let ron = "https://www.linkedin.com/in/ron-peleg-8175b0173"
+        let gilad = "https://www.linkedin.com/in/gilad-shacham/"
+        let GPT4 = "https://openai.com/product/gpt-4"
 
         return(
             <div>
@@ -13,7 +15,7 @@ export class Footer extends React.PureComponent {
 
                     <p>סמסטר {ConstsDict.currentSemester === 1 ? "א" : "ב"}׳ | {ConstsDict.semesterLastDay.slice(0,4)}</p>
                     <p>
-                        Created by <a href={omri}>Omri Fahn</a> and wouldn't be possible without the help of <a href={ido}>Ido Weinstein</a> and <a href={ron}>Ron Peleg</a>.
+                        Created by <a href={omri}>Omri Fahn</a> and wouldn't be possible without the help of <a href={ido}>Ido Weinstein</a>, <a href={ron}>Ron Peleg</a>, <a href={gilad}>Gilad Shacham</a> and our beloved <a href={GPT4}>GPT-4</a>.
                     </p>
                     <p>TAU-Cal © {ConstsDict.semesterLastDay.slice(0,4)}</p>
                 </footer >
