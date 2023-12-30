786ff98df6eb1ef51a554b7224a02d0c2fec53d1
omrifahn <omrifahn@gmail.com>
2023-12-30 12:08:59 +0200

    dates update

--- src/components/Calendar_components/Consts.js
+++ src/components/Calendar_components/Consts.js
@@ -1,7 +1,7 @@
 const ConstsDict = {
     currentSemester: 1,
-    semesterFirstDay : "20231015",
-    semesterLastDay : "20240115"
+    semesterFirstDay : "20231231",
+    semesterLastDay : "20240315"
 }// todo - separate to SemFirstDay.DD SemFirstDay.MM SemFirstDay.YY
 
 export default ConstsDict;
